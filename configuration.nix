0559af1c6a96e70523e2f0e8f0d735992a336c14
Wes Gray <wes.gray@gmail.com>
2025-07-19 00:22:43 -0700

    Don't track git metadata

--- configuration.nix
+++ configuration.nix
@@ -204,5 +204,12 @@ Host *
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
+  
+  system = {
+    nixos = {
+      label = "no_git_metadata";
+      version = "no_git_metadata";
+    };
+    tools.nixos-version.enable = false;
+  };
 }
